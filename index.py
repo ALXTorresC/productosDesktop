711bdd1b4e085564e9026febb16e8eb900d2610d
Alexis Torres <alexis.torres@genesys.cl>
2019-09-12 10:44:50 -0300

    "Creación del primer input"

--- index.py
+++ index.py
@@ -11,6 +11,15 @@ class Product:
         self.wind = window
         self.wind.title = "Catalogo de productos."
 
+        # Creando un contenedor para agregar elementos.
+        frame = LabelFrame(self.wind, text = "Registrar un nuevo producto")
+        frame.grid(row=0, column = 0, columnspan = 3, pady=20 )
+    
+        # Input
+        Label(frame, text="Nombre: ").grid(row=1,column=0)
+        self.name = Entry(frame)
+        self.name.grid(row=1, column=1)
+
 if __name__=='__main__':
     window = Tk()
     application = Product(window)
