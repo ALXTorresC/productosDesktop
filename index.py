66f0fc83b841838e0a30ebbdce738d27dfc96d16
Alexis Torres <alexis.torres@genesys.cl>
2019-09-13 18:11:21 -0300

    Actualización del archivo

--- index.py
+++ index.py
@@ -5,6 +5,7 @@ from tkinter import *
 
 import sqlite3
 
+
 class Product:
 
     def __init__(self, window):
@@ -13,12 +14,27 @@ class Product:
 
         # Creando un contenedor para agregar elementos.
         frame = LabelFrame(self.wind, text = "Registrar un nuevo producto")
-        frame.grid(row=0, column = 0, columnspan = 3, pady=20 )
+        frame.grid(row=0, column = 0, columnspan = 3, padx=20, pady=20)
     
         # Input
-        Label(frame, text="Nombre: ").grid(row=1,column=0)
+        Label(frame, text="Nombre:").grid(row=1, column=0, pady=10, padx=5)
         self.name = Entry(frame)
-        self.name.grid(row=1, column=1)
+        self.name.focus()
+        self.name.grid(row=1, column=1,padx=5)
+
+        # Price input
+        Label(frame, text="Precio:").grid(row=2, column=0, pady=5, padx=5)
+        self.price = Entry(frame)
+        self.price.grid(row=2, column=1, padx=5)
+
+        # Botón del formulario
+        ttk.Button(frame, text="Guardar producto").grid(row=3, pady=5, padx=5, columnspan=2, sticky = W + E)
+
+        # Arbol de vista
+        self.tree = ttk.Treeview(height = 10, columns = 2)
+        self.tree.grid( row = 4, column = 0, columnspan = 2)
+        self.tree.heading('#0', text="Nombre", anchor = CENTER)
+        self.tree.heading('#1', text="Precio", anchor = CENTER)
 
 if __name__=='__main__':
     window = Tk()
